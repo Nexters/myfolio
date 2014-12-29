999e14927d354ce9a58c58e44601ea19ac413ccb
Donghyun Go <godong9@gmail.com>
2014-12-29 13:39:32 +0900

    Update Main.js

--- server/controllers/Main.js
+++ server/controllers/Main.js
@@ -1,13 +1,18 @@
 var BaseController = require("./Base");
     //model = new (require("../models/MainModel"));
 
-var MainController = new BaseController("MainController");
+function MainController() {
 
-MainController.run = function(req, res, next) {
+}
+
+MainController.prototype = new BaseController("MainController");
+
+MainController.prototype.run = function(req, res, next) {
     var self = this;
     self.content = {testData: "Test Data"};
     res.render('Main.ejs',self.content);
 };
+
 
 module.exports = MainController;
 
