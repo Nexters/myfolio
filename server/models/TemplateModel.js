45a6b595808ecfcf098a608faba7611d8e6979bc
Donghyun Go <godong9@gmail.com>
2015-01-22 21:11:49 +0900

    Update TemplateModel

--- server/models/TemplateModel.js
+++ server/models/TemplateModel.js
@@ -10,7 +10,7 @@ function TemplateModel() {
 }
 
 TemplateModel.prototype.selectAll = function (criteria, options, callback) {
-    var query = "SELECT * FROM TEMPLATE_TB WHERE NOT TEMPLATE_ID=0";
+    var query = "SELECT * FROM TEMPLATE_TB;";
 
     pool.query(query, function (err, rows, fields) {
         callback(err, rows);
