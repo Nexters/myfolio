ce93148e1ba349331a9c619baf5fb2feeb4fb8c1
Donghyun Go <godong9@gmail.com>
2015-01-24 13:56:02 +0900

    Update user.sql

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,6 @@ var router = express.Router();
 var mainController = new (require('../controllers/Main'))();
 var portfolioController = new (require('../controllers/Portfolio'))();
 
-
 /* GET main page */
 router.get('/', mainController.run);
 
