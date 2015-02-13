55e66cc8086b4c7fc6a922453185fb6418873122
이윤석 <pineoc@naver.com>
2015-02-14 02:19:30 +0900

    first part done

--- server/public/javascripts/template/template3.js
+++ server/public/javascripts/template/template3.js
@@ -1,16 +1,26 @@
-function navFunction(tag){
-    switch (tag){
-        case 1:
-            document.getElementsByName("first");
-            window.scrollTo(0,0);
-            break;
-        case 2:
-            window.scrollTo(0,700);
-            break;
-        case 3:
-            window.scrollTo(0,1800);
-            break;
-        default:
-            break;
+(function template2() {
+    'use strict';
+    function addnavBtn(tag){
+        $('#template_editor_save_btn').on('TEMPLATE_SAVE_EVENT', function(e) {
+            console.log("SAVE");
+        });
+        switch (tag){
+            case 1:
+                window.scrollTo(0,0);
+                break;
+            case 2:
+                window.scrollTo(0,700);
+                break;
+            case 3:
+                window.scrollTo(0,1800);
+                break;
+            default:
+                break;
+        }
     }
-}
+
+    function initTemplate3() {
+        //init datas
+    }
+    initTemplate3();
+}());
