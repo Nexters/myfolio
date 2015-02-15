5d6688174035a81add3564606f2c2ba38319742e
이윤석 <pineoc@naver.com>
2015-02-15 12:03:51 +0900

    template3 commit, edit mode before start

--- server/public/javascripts/template/template3.js
+++ server/public/javascripts/template/template3.js
@@ -18,9 +18,29 @@
                 break;
         }
     }
+    function addImageChangeEvent() {
+        $('.image-file-upload-btn').unbind('fileupload').fileupload({
+            dataType: 'json',
+            progressall: function (e, data) {
+                console.log("progress");
+            },
+            done: function (e, data) {
+                var uploadedImageUrl = "/image/" + data.result;
+                $(this).parent().children('.view-image-item').attr('href',uploadedImageUrl);
+                $(this).parent().children('.view-image-item').children('img').attr('src',uploadedImageUrl);
+            }
+        });
+    }
+    function addImageDeleteEvent() {
+        $('.image-delete-btn').unbind('click').click(function() {
+            $(this).parent().remove();
+        });
+    }
 
     function initTemplate3() {
         //init datas
+        addImageChangeEvent();
+        addImageDeleteEvent();
     }
     initTemplate3();
 }());
