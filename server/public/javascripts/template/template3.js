52899f35833b3490b4e5a995d9b6b6ce29b3e361
이윤석 <pineoc@naver.com>
2015-02-15 15:52:28 +0900

    img btn on part one, edit mode going on

--- server/public/javascripts/template/template3.js
+++ server/public/javascripts/template/template3.js
@@ -1,5 +1,7 @@
-(function template2() {
+(function template3() {
     'use strict';
+    var count=0;
+    var img_arr = [];
     function addnavBtn(tag){
         $('#template_editor_save_btn').on('TEMPLATE_SAVE_EVENT', function(e) {
             console.log("SAVE");
@@ -17,6 +19,12 @@
             default:
                 break;
         }
+    }
+    function initViewImageItem() {
+        $('a.view-image-item').each(function(idx, item) {
+            $(item).attr('href', $(item).data('image'));
+            $(item).children('img').attr('src', $(item).data('image'));
+        });
     }
     function addImageChangeEvent() {
         $('.image-file-upload-btn').unbind('fileupload').fileupload({
@@ -36,11 +44,37 @@
             $(this).parent().remove();
         });
     }
+    function addImageNextEvent(){
+        img_arr.push($('#img_item1').attr('src'));
+        img_arr.push($('#img_item2').attr('src'));
+        img_arr.push($('#img_item3').attr('src'));
+        $('#right_btn').bind('click',function(){
+            console.log('count : ',count);
+            if(count>=0 && count<2){
+                count++;
+                $('.imgs').css('background','url("' + img_arr[count] + '") no-repeat');
+                $('.imgs').css('background-size', 'cover');
+            }
+        });
+    }
+    function addImageBeforeEvent(){
+        $('#left_btn').bind('click',function(){
+            console.log('count : ',count);
+            if(count>=1 && count<3){
+                count--;
+                $('.imgs').css('background','url("' + img_arr[count] + '") no-repeat');
+                $('.imgs').css('background-size', 'cover');
+            }
+        });
+    }
 
     function initTemplate3() {
         //init datas
+        initViewImageItem();
         addImageChangeEvent();
         addImageDeleteEvent();
+        addImageNextEvent();
+        addImageBeforeEvent();
     }
     initTemplate3();
 }());
