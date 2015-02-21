657409427d0f8d3181a9c198f34103978be08daf
Donghyun Go <godong9@gmail.com>
2015-02-21 09:35:36 +0900

    Update favicon

--- server/app.js
+++ server/app.js
@@ -23,7 +23,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,6 +30,7 @@ app.use(cookieParser());
 
 // Session
 if (app.get('env') === 'production') {
+    app.use(favicon(__dirname + '/dist/favicon.ico'));
     app.use(express.static(path.join(__dirname, 'dist')));
     MongoStore = require('connect-mongo')(session);
     app.use(session({
@@ -40,6 +40,7 @@ if (app.get('env') === 'production') {
         saveUninitialized: true
     }));
 } else {
+    app.use(favicon(__dirname + '/public/favicon.ico'));
     app.use(express.static(path.join(__dirname, 'public')));
     app.use(session({
         secret: 'myfolio-session',
